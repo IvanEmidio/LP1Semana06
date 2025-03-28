f23098500139b07e11380991b577ffc69b7f6f90
IvanEmidio <ivan.emidio@live.com.pt>
2025-03-28 05:55:22 +0000

    Completed RandDice exercise

---
+++ RandDice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            Random random= new Random(s);
+
+            int total = 0;
+
+
+            for(int i = 0; i < n; i++)
+            {
+                total += random.Next(1,7);
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
