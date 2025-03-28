14647a98b2d5c6af2045e3fc0959336a0c76697a
IvanEmidio <ivan.emidio@live.com.pt>
2025-03-28 14:29:20 +0000

    Saved MyRPG class

--- MyRPG/MyRPG.cs
+++ MyRPG/MyRPG.cs
@@ -5,8 +5,59 @@ using System.Threading.Tasks;
 
 namespace MyRPG
 {
-    public class NewClass
+    public class Enemy
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if(shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if(health < 0) health = 0;
+            }
+        }
+
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public void SetName(string name)
+        {
+            int size = 8;
+
+            if(name.Length < size)
+            {
+                this.name = name;
+            }
+            else
+            {
+                this.name = name.Substring(0, size);
+            }
+        }
     }
 }
